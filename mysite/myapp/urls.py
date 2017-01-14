22e1c313ed228fc6a43f1ea6f6acb73335231ebc
maram <you@example.com>
2017-01-14 14:03:45 +0200

    upload file

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
 from . import views
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
@@ -11,4 +15,4 @@ urlpatterns = [
 	url(r'^profile/(?P<username>[a-zA-Z0-9]+)$',views.profile , name='profile'),
 	url(r'^upload/$' , views.upload_file ,name='upload_file'),
 	url(r'^success/$' , views.success_upload ,name ='success_upload'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
